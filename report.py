626469d7f4dd7a6975810494a42f687e32c9af3c
Arpit Garg <bluearpit@gmail.com>
2018-03-29 23:05:09 -0700

    fix sort bug using test data

--- report.py
+++ report.py
@@ -1,13 +1,14 @@
 from sys import argv
-from time import localtime, strftime
+from time import localtime, strftime, strptime
 
 from operator import itemgetter
 
 '''
 The complexity of the code wil be:
-O(mlogm * klogk) where m is len of unique dates sorted and k is len of sorted url hits.
-Since we are given that unique dates and hits are pretty low and unique urls fit into memory,
-this code wouldn't take too long.
+O(n+ mlogm * klogk) where n is number of lines in input file, m is number of
+unique dates sorted and k is len of sorted url hits.
+Since we are given that unique dates and hits are pretty low (n >> k),
+essentially the complexity should be O(n) as it will take most of the time.
 '''
 
 
@@ -35,7 +36,8 @@ def create_url_map(input_file):
     with open(input_file) as _file:
         for line in _file:
             line = line.split('|')
-            gmt = strftime('%m/%d/%Y', localtime(int(line[0])))
+            gmt = strftime('%Y-%m-%d', localtime(int(line[0])))
+            # gmt = line[0][:10]
             if not in_dict(time_url_map, gmt):
                 time_url_map[gmt] = {}
             url = line[1].strip('\n')
@@ -53,11 +55,17 @@ def print_sorted_mapping(time_url_map):
     times each url is visited in the order of highest hit count to lowest count.
     :param dictionary:
     """
+
+    # with open('sorted_output.txt','w') as sortf:
     for i in sorted(time_url_map.items(), key=itemgetter(0)):
-        print(i[0] + ' GMT')
+        new_date = strftime('%m/%d/%Y', strptime(i[0], '%Y-%m-%d'))
+        print(new_date + ' GMT')
+        # sortf.write(str(new_date) + ' GMT' + '\n')
         sorted_urls_count = sorted(i[1].items(), key=itemgetter(1), reverse=True)
         for j in sorted_urls_count:
+            # sortf.write(j[0] + ' ' + str(j[1]) + '\n')
             print(j[0] + ' ' + str(j[1]))
+            # sortf.write('------------------------------------------------------------------------------------\n')
 
 
 def main(input_file):
